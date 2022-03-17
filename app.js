2dfde373b4e1e51d8b50711d5d03ebf3f23bd4fb
Raj Prajapati <rajce604@gmail.com>
2022-03-17 14:41:50 -0400

    Create NFT API

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const fetch = require("node-fetch");
 const path = require("path");
 const { create } = require("ipfs-http-client");
 const { url } = require("inspector");
+const { json } = require("express/lib/response");
 const axios = require("axios").default;
-async function ipfsClient() {
-  let ipfs = await create({
-    host: "ipfs.infura.io",
-    port: 5001,
-    protocol: "https",
-  });
-  return ipfs;
+// async function ipfsClient() {
+//   let ipfs = await create({
+//     host: "ipfs.infura.io",
+//     port: 5001,
+//     protocol: "https",
+//   });
+//   return ipfs;
+// }
+
+async function getDataBlob(imageUrl) {
+  var res = await fetch(imageUrl);
+  var blob = await res.blob();
+  var obj = {};
+  obj.contentType = blob.type;
+
+  var buffer = await blob.arrayBuffer();
+  obj.data = buffer;
+
+  return obj;
 }
 
 const app = express();
@@ -52,6 +66,98 @@ app.post("/downloadNFT", async (req, res) => {
   //   console.log(data);
   // }
   //res.status(200).json({ result: result });
+});
+
+app.post("/createNFT", async (req, res) => {
+  const cid = req.body.cid;
+  const walletAddress = req.body.walletAddress;
+  const walletData = JSON.stringify(req.body.walletData);
+  const title = req.body.title;
+  const name = req.body.name;
+  const description = req.body.description;
+
+  console.log("Walletdata", walletData);
+
+  fs.writeFile(
+    __dirname + "/tempfile/" + walletAddress,
+    walletData,
+    function (err) {
+      if (err) throw err;
+      console.log("File created succesfully");
+    }
+  );
+
+  const url = "https://ipfs.io/ipfs/" + cid;
+  console.log("IMAGEURL", url);
+  let imageOBJ = await getDataBlob(
+    url // paste your image url here
+  );
+  const initialState = {
+    title: title,
+    name: name,
+    description: description,
+    ticker: "KOINFT",
+    balances: {
+      walletAddress: 1,
+    },
+    owners: {
+      1: json.stringify(walletAddress),
+    },
+    maxSupply: 5,
+    locked: [],
+    contentType: "image/png",
+    createdAt: Date.now,
+    tags: [""],
+  };
+  let tx;
+  let wallet = JSON.parse(
+    fs.readFileSync(__dirname + "/tempfile/" + walletAddress, "utf-8")
+  );
+  try {
+    tx = await arweave.createTransaction(
+      {
+        data: imageOBJ.data,
+      },
+      wallet
+    );
+  } catch (err) {
+    console.log("create transaction error");
+    console.log("err-transaction", err);
+    return false;
+  }
+
+  tx.addTag("Content-Type", imageOBJ.contentType);
+  tx.addTag("Network", "Raj");
+  tx.addTag("Action", "marketplace/Create");
+  tx.addTag("App-Name", "SmartWeaveContract");
+  tx.addTag("App-Version", "0.3.0");
+  tx.addTag("Contract-Src", "r_ibeOTHJW8McJvivPJjHxjMwkYfAKRjs-LjAeaBcLc");
+  tx.addTag("Init-State", JSON.stringify(initialState));
+  try {
+    await arweave.transactions.sign(tx, wallet);
+  } catch (err) {
+    console.log("transaction sign error");
+    console.log("err-sign", err);
+    return false;
+  }
+  try {
+    // console.log(" wallet : ", wallet);
+    console.log("TX", tx.id);
+    let uploader = await arweave.transactions.getUploader(tx);
+    console.log("uploder", uploader);
+
+    while (!uploader.isComplete) {
+      await uploader.uploadChunk();
+      console.log(
+        uploader.pctComplete + "% complete",
+        uploader.uploadedChunks + "/" + uploader.totalChunks
+      );
+    }
+    res.status(200).json({ Id: tx.id });
+  } catch (err) {
+    console.log("err-last", err);
+    return false;
+  }
 });
 
 app.listen(3000);
---
+++ images/readme.md
@@ -0,0 +1,1 @@
+This folder contains the image downloaded from IPFS
